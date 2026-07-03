9e178d142dac78dfdf379e69ffbee86aff1cde57
Syed Basit Ali <Basitali.vision@gmail.com>
2026-07-04 02:41:56 +0500

    Refactor layout component and rename to UsersLayout

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -1,53 +1,20 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Syed Basit Ali | Frontend Developer",
-  description: "Frontend Developer specializing in building high-performance web applications using React, Next.js, and Tailwind CSS.",
-  icons: {
-    icon: "/imagge.png"
-  },
-  verification: {
-    google: "k8JSUpxTAAwsmoIJPvRxbGSlXbIipqrXEcQKypBjHAo", 
-  },
-};
-
-export default function RootLayout({ children }) {
+export default function UsersLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        {/* Direct Framework Bypass Tag */}
-        <meta name="google-site-verification" content="k8JSUpxTAAwsmoIJPvRxbGSlXbIipqrXEcQKypBjHAo" />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-         <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <Header />
-            {children}
-            <Toaster position="top-center"/>
-            <Footer />
-         </ThemeProvider>
-      </body>
-    </html>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Header />
+      {children}
+      <Toaster position="top-center"/>
+      <Footer />
+    </ThemeProvider>
   );
 }
