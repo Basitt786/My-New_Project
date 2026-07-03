6843b960c6dd2cd968695c6cad7ebfd69829a27a
Syed Basit Ali <Basitali.vision@gmail.com>
2026-07-04 02:46:54 +0500

    Integrate Google Fonts and update layout metadata
    Added Google Fonts and metadata for SEO.

--- app/(users)/layout.js
+++ app/(users)/layout.js
@@ -1,20 +1,53 @@
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../globals.css"; // sub-folder mein hone ki wajah se yahan ../ chalega
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
-export default function UsersLayout({ children }) {
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Syed Basit Ali | Frontend Developer",
+  description: "Frontend Developer specializing in building high-performance web applications using React, Next.js, and Tailwind CSS.",
+  icons: {
+    icon: "/imagge.png"
+  },
+  verification: {
+    google: "k8JSUpxTAAwsmoIJPvRxbGSlXbIipqrXEcQKypBjHAo", 
+  },
+};
+
+export default function RootLayout({ children }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <Header />
-      {children}
-      <Toaster position="top-center"/>
-      <Footer />
-    </ThemeProvider>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Verification Direct Meta Tag */}
+        <meta name="google-site-verification" content="k8JSUpxTAAwsmoIJPvRxbGSlXbIipqrXEcQKypBjHAo" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+         <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            {children}
+            <Toaster position="top-center"/>
+            <Footer />
+         </ThemeProvider>
+      </body>
+    </html>
   );
 }
